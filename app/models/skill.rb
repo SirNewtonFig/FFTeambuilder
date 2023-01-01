cbc67f1a1bc0ff45988ec059d8fbc8061acd372b
SirNewtonFig <sirnewtonfig@gmail.com>
2023-01-01 13:11:49 -0400

    Enforce skill uniqueness on RSM + primary/secondary

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -6,8 +6,16 @@ class Skill < ApplicationRecord
 
   scope :valid, ->(char) {
     s = joins(:job).merge(Job.valid(char))
-    s = s.where('1=0') if char.data['job'].to_i == 20
+    s = s.where('1=0') unless char.can_equip_skills?
 
     s
+  }
+
+  scope :unique, ->(char, team) {
+    other_skills = (team.characters - [char]).map { |c|
+      [c.data['reaction'], c.data['support'], c.data['movement'], *c.primary_skill_ids, *c.secondary_skill_ids]
+    }.compact.flatten.map(&:to_i)
+
+    where.not(id: other_skills)
   }
 end
