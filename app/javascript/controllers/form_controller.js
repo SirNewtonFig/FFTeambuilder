bf78d198ce908d3df29a900d0dade6a41ee66ff6
SirNewtonFig <sirnewtonfig@gmail.com>
2023-01-14 08:38:25 -0400

    Fix submit events

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class FormController extends Controller {
   submit() {
-    this.element.dispatchEvent(new CustomEvent('submit', { bubbles: true }))
+    this.element.requestSubmit()
   }
 }
