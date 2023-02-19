b7bcfe2aed2e4336127b11ae011cfc555dd5fc44
SirNewtonFig <sirnewtonfig@gmail.com>
2023-02-19 18:15:37 -0400

    Add tooltips for skills

--- app/helpers/stat_helper.rb
+++ app/helpers/stat_helper.rb
@@ -2,7 +2,14 @@ module StatHelper
   LABELS = {
     'attack' => 'PA',
     'magic' => 'MA',
-    'speed' => 'Sp'
+    'speed' => 'Sp',
+    'mp' => 'MP',
+    'ct' => 'CT',
+    'silence' => 'Silence?',
+    'counter' => 'Counter?',
+    'reflectable' => 'Reflect?',
+    'counter_flood' => 'Counter Flood?',
+    'counter_magic' => 'Counter Magic?'
   }
 
   def stat_label(stat)
