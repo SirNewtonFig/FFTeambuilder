d37772b36ad3c0505747266f94ac4f975537f039
SirNewtonFig <sirnewtonfig@gmail.com>
2025-01-23 11:27:42 -0400

    Show and manage submission deadline

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,12 +8,28 @@ class EventsController < ApplicationController
     render layout: 'modal_neutral'
   end
 
+  def edit
+    @event = Event.find(params[:id])
+
+    raise 'unauthorized' unless @event.user == Current.user
+
+    render layout: 'modal_neutral'
+  end
+
   def create
     @event = Event.new(params.require(:event).permit(:title, :deadline, :active))
 
     @event.update(user: current_user)
 
     @events = Event.active.open.order(:deadline)
+  end
+
+  def update
+    @event = Event.find(params[:id])
+
+    @event.update(params.require(:event).permit(:title, :deadline, :active))
+
+    redirect_to event_path(@event)
   end
 
   def show
