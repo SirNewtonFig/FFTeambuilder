a821815c031613b8a89724e20f952e955387e7b1
SirNewtonFig <sirnewtonfig@gmail.com>
2025-01-22 22:50:57 -0400

    Only initialize discord omniauth post-deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,9 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :discord, Rails.application.credentials.discord.client_id, Rails.application.credentials.discord.client_secret
+  if Rails.application.credentials.discord
+    config.omniauth :discord, Rails.application.credentials.discord.client_id, Rails.application.credentials.discord.client_secret
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
