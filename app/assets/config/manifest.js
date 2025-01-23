5c52fb02506272d95e4d402a107bc834dbe47208
SirNewtonFig <sirnewtonfig@gmail.com>
2025-01-22 23:23:14 -0400

    Link bundled administrate assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -3,5 +3,7 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
+//= link administrate/application.css
+//= link administrate/application.js
 //= link administrate-field-jsonb/application.css
 //= link administrate-field-jsonb/application.js
